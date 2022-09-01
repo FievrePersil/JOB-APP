aaadedabd43195b65c1db99997371757933fb7d4
Programming Hints <tarek-tiko@live.fr>
2022-09-01 14:15:56 +0100

    3rd

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Index from './Index'
+import Index from './Index';
 import Contact from './Contact';
 import Register from './Register';
 import Login from './Login'
